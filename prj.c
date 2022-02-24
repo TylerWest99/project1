3b4dfe3b2b0d95b1ce085f02782b02a7dac4d7c6
Tyler West <tylerwest@Tylers-MacBook-Pro.local>
2022-02-24 16:25:36 -0500

    adds user input

--- prj.c
+++ prj.c
@@ -1,1 +1,22 @@
 // code for project 1 by Jacob and Tyler
+
+//includes are below
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <semaphore.h>
+#include <signal.h>
+
+int main(){
+
+	int k;
+	printf("How many nodes would you like to spawn?");
+	scanf("%d", &k);
+	printf("k is... %d\n", k); 
+
+}
